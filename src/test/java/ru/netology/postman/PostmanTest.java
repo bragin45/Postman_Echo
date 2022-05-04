7fe354c741dd6738b5c6d87a2c22baeb4a2af3b5
Bragin Aleksey <tech45ru@gmail.com>
2022-05-04 23:19:21 +0500

    Update test

--- src/test/java/ru/netology/postman/PostmanTest.java
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -18,7 +18,7 @@ public class PostmanTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("test postman_echo wrong"))
+                .body("data", equalTo("test postman_echo"))
                 ;
     }
 
