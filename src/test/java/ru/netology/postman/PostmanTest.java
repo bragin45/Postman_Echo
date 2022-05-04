b94e59b7bc3a1ff71f79a924ccb84ff477bd637c
Bragin Aleksey <tech45ru@gmail.com>
2022-05-04 23:06:02 +0500

    Added appveyor.yml

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void shouldPostmanEcho () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test postman_echo")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test postman_echo"))
+                ;
+    }
+
+}
